03eb3dac4b95f16a6b83fc6a749222f455f2afc9
JOÃO CARLOS FORMENTO <joao_c_formento@estudante.sesisenai.org.br>
2025-05-14 17:07:07 -0300

    Inicio da página login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,36 @@
-export const LoginPage = () =>{
-    return(
-        <div className="flex flex-col justify-center items-center h-[100%] w-[100%]"><h1>login</h1></div>
-    )
-}
+export const LoginPage = () => {
+  return (
+    <div className="flex flex-col justify-center items-center h-[100%] w-[100%]">
+      <form
+        action=""
+        className="flex flex-col justify-center items-center h-[90%] w-[60%]"
+      >
+        <div className="w-[100%] h-[100%]">
+          <label htmlFor="email"> E-mail </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            required
+            className="focus:outline-none w-[30%] h-[10%] bg-gray-700 rounded-sm"
+          />
+          <label htmlFor="password"> Senha</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            required
+            className="focus:outline-none w-[30%] h-[10%] bg-gray-700 rounded-sm"
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="w-[30%] h-[10%] bg-blue-500 rounded-sm"
+        >
+          Entrar
+        </button>
+      </form>
+    </div>
+  );
+};
